81f9d235d3007f0c3e62127964eaf21fd925f1ea
mfabianchm <mfchongm@hotmail.com>
2025-08-19 09:25:23 -0600

    "creation of project and configuration for application.properties"

---
+++ src/main/java/com/example/moneymanagerApp/MoneymanagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneymanagerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymanagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymanagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moneymanagerApp/MoneymanagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneymanagerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymanagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
